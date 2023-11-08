d6b33a52d821c5b315eca61e6c33fba2e70acc82
Lucas Lucena <lucas.lucenak@gmail.com>
2023-11-07 23:53:33 -0300

    Add login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  {path: 'delivery', component: DeliveryComponent}
+  {path: 'delivery', component: DeliveryComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
