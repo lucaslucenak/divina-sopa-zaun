18e33ca192af93a0ac0e082cae7dd78e5200892b
Daniel Xavier <dxavier194@gmail.com>
2023-11-10 17:36:29 -0300

    Criado o menu da navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  showHeaderInfo = true;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const threshold = 100; // Exemplo de limite de rolagem para mudar a visibilidade
+    this.showHeaderInfo = (window.pageYOffset < threshold);
+  }
 }
