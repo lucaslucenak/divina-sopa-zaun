8bb93af2be342dc6b2f4e39d8d1c236cab01b7b4
Daniel Xavier <dxavier194@gmail.com>
2023-11-13 20:53:52 -0300

    Adicionado botão, estilo e ato de abrir no navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,10 +7,17 @@ import { Component, HostListener } from '@angular/core';
 })
 export class HeaderComponent {
   showHeaderInfo = true;
+  showSearchBar = false;
+  searchTerm: string = '';
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const threshold = 100; // Exemplo de limite de rolagem para mudar a visibilidade
     this.showHeaderInfo = (window.pageYOffset < threshold);
+  }
+
+  toggleSearch(): void {
+    this.showSearchBar = !this.showSearchBar;
+    // Lógica
   }
 }
